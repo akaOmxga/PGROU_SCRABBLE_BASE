3fadba2b60b0a4229a72f984870c5a76c9e1d596
nekouki <nour-elhouda.kouki@eleves.ec-nantes.fr>
2025-03-12 12:24:45 +0100

    classe joueur

--- public/js/objet/Joueur.js
+++ public/js/objet/Joueur.js
@@ -1,110 +1,56 @@
-
-// Classe Joueur :
-// possède en attribut son score, son inventaire de lettres
-// méthode : il peut jouer le tour (poser un mot), le valider, ou passer son tour
-
 export class Joueur {
 
-    constructor(id,playerPseudo) {
+    constructor(id, playerPseudo) {
         this.id = id;
         this.lettres = [];  // Inventaire limité à 7 lettres
         this.pseudo = playerPseudo;
         this.score = 0;
-
     }
 
-    // Ajoute une lettre à l'inventaire si possible
     ajouterLettre(lettre) {
-
-        this.lettres.push(lettre);
-
+        if (this.lettres.length < 7) {
+            this.lettres.push(lettre);
+        }
     }
 
-    // Retire une lettre spécifique de l'inventaire
     retirerLettre(valeurLettre) {
         const index = this.lettres.findIndex(l => l.valeur === valeurLettre);
         if (index > -1) {
             this.lettres.splice(index, 1);
             return true;
-            // Firebase: Mettre à jour l'inventaire du joueur
         }
         return false;
     }
-    
 
-    // Vérifie si le joueur a les lettres nécessaires pour former un mot
     possedeLettre(mot) {
         const lettresTemp = [...this.lettres];
         for (let lettre of mot.toUpperCase()) {
-            // On cherche l'objet dont la propriété "valeur" correspond à la lettre
             const index = lettresTemp.findIndex(l => l.valeur === lettre);
             if (index === -1) {
-              // Même chose pour le joker :
-              const jokerIndex = lettresTemp.findIndex(l => l.valeur === '*');
-              if (jokerIndex === -1) return false;
-              lettresTemp.splice(jokerIndex, 1);
+                const jokerIndex = lettresTemp.findIndex(l => l.valeur === '*');
+                if (jokerIndex === -1) return false;
+                lettresTemp.splice(jokerIndex, 1);
             } else {
-              lettresTemp.splice(index, 1);
+                lettresTemp.splice(index, 1);
             }
-          }
+        }
         return true;
     }
 
-    jouerMot(mot, position, direction, plateau) {
-        // Vérifier si le joueur a les lettres nécessaires
+    jouerMot(mot, position, direction, plateau, pioche) {
         if (!this.possedeLettre(mot)) {
             return false;
         }
 
-        // Vérifier si le mot est valide dans le dictionnaire
-        // TODO: Implémenter la vérification avec la base de données externe
-        /* 
-        checkWordInDictionary(mot).then(isValid => {
-            if (!isValid) return false;
-        });
-        */
-    
-        // Si le mot est valide, Retirer les lettres utilisées du joueur (en comparant par valeur)
         for (let lettre of mot.toUpperCase()) {
             this.retirerLettre(lettre);
         }
-    
-        // Debug : vérifier l'état de l'inventaire après retrait
-        console.log("Inventaire après retrait du mot :", this.lettres.map(l => l.valeur));
-    
-        // Placer le mot sur le plateau
+
         plateau.placerMot(mot, position, direction);
-    
-        // Firebase: Mettre à jour l'état du plateau et l'inventaire du joueur
+        this.completerLettres(pioche);
+
         return true;
-    }    
-    
-//   jouerMot(mot, position, direction, plateau) {
-        // Vérifier si le joueur a les lettres nécessaires
-//        if (!this.possedeLettre(mot)) {
-//            return false;
-//        }
-
-        // Vérifier si le mot est valide dans le dictionnaire
-        // TODO: Implémenter la vérification avec la base de données externe
-        /* 
-        checkWordInDictionary(mot).then(isValid => {
-            if (!isValid) return false;
-        });
-        */
-
-        // Si le mot est valide, retirer les lettres utilisées
-//        for (let lettre of mot.toUpperCase()) {
-//            this.retirerLettre(lettre);
-//        }
-
-        // Placer le mot sur le plateau
-//        plateau.placerMot(mot, position, direction);
-
-        // Firebase: Mettre à jour l'état du plateau et l'inventaire du joueur
-//        return true;
-//    } 
-
+    }
 
     completerLettres(pioche) {
         while (this.lettres.length < 7 && !pioche.estVide()) {
@@ -113,13 +59,9 @@ export class Joueur {
                 this.ajouterLettre(lettre);
             }
         }
-
     }
 
     passerTour() {
-        // Logique pour passer son tour
-        // Firebase: Notifier que le joueur passe son tour
         return true;
     }
 }
-
